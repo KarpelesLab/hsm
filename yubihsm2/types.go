ff0791b64da2bd9e329f304ed3adccb5355645f4
Mark Karpeles <magicaltux@gmail.com>
2019-04-17 16:53:11 +0900

    added stringers & fixed types

--- yubihsm2/types.go
+++ yubihsm2/types.go
@@ -16,6 +16,8 @@ var (
 	ErrInvalidResponseType = errors.New("yubihsm2: response has invalid type")
 )
 
+//go:generate stringer -type=Capability,ErrorCode,CommandType,ObjectType,Domain -output types_s.go
+
 const (
 	ResponseCommandOffset = 0x80
 	ErrorResponseCode     = 0xff
@@ -87,80 +89,84 @@ const (
 	ErrObjectNotFound    ErrorCode = 0x0b
 	ErrIDIllegal         ErrorCode = 0x0c
 	ErrCommandUnexecuted ErrorCode = 0xff
+)
 
+const (
 	// Capabilities
 	GetOpaque               Capability = 0x0000000000000001
-	PutOpaque                          = 0x0000000000000002
-	PutAuthKey                         = 0x0000000000000004
-	PutAsymmetric                      = 0x0000000000000008
-	AsymmetricGen                      = 0x0000000000000010
-	AsymmetricSignPkcs                 = 0x0000000000000020
-	AsymmetricSignPss                  = 0x0000000000000040
-	AsymmetricSignEcdsa                = 0x0000000000000080
-	AsymmetricSignEddsa                = 0x0000000000000100
-	AsymmetricDecryptPkcs              = 0x0000000000000200
-	AsymmetricDecryptOaep              = 0x0000000000000400
-	AsymmetricDecryptEcdh              = 0x0000000000000800
-	ExportWrapped                      = 0x0000000000001000
-	ImportWrapped                      = 0x0000000000002000
-	PutWrapKey                         = 0x0000000000004000
-	GenerateWrapKey                    = 0x0000000000008000
-	ExportUnderWrap                    = 0x0000000000010000
-	PutOption                          = 0x0000000000020000
-	GetOption                          = 0x0000000000040000
-	GetRandomness                      = 0x0000000000080000
-	PutHmacKey                         = 0x0000000000100000
-	HmacKeyGenerate                    = 0x0000000000200000
-	HmacData                           = 0x0000000000400000
-	HmacVerify                         = 0x0000000000800000
-	Audit                              = 0x0000000001000000
-	SshCertify                         = 0x0000000002000000
-	GetTemplate                        = 0x0000000004000000
-	PutTemplate                        = 0x0000000008000000
-	Reset                              = 0x0000000010000000
-	OtpDecrypt                         = 0x0000000020000000
-	OtpAeadCreate                      = 0x0000000040000000
-	OtpAeadRandom                      = 0x0000000080000000
-	OtpAeadRewrapFrom                  = 0x0000000100000000
-	OtpAeadRewrapTo                    = 0x0000000200000000
-	Attest                             = 0x0000000400000000
-	PutOtpAeadKey                      = 0x0000000800000000
-	GenerateOtpAeadKey                 = 0x0000001000000000
-	WrapData                           = 0x0000002000000000
-	UnwrapData                         = 0x0000004000000000
-	DeleteOpaque                       = 0x0000008000000000
-	DeleteAuthKey                      = 0x0000010000000000
-	DeleteAsymmetric                   = 0x0000020000000000
-	DeleteWrapKey                      = 0x0000040000000000
-	DeleteHmacKey                      = 0x0000080000000000
-	DeleteTemplate                     = 0x0000100000000000
-	DeleteOtpAeadKey                   = 0x0000200000000000
-	ChangeAuthenticationKey            = 0x0000400000000000
+	PutOpaque               Capability = 0x0000000000000002
+	PutAuthKey              Capability = 0x0000000000000004
+	PutAsymmetric           Capability = 0x0000000000000008
+	AsymmetricGen           Capability = 0x0000000000000010
+	AsymmetricSignPkcs      Capability = 0x0000000000000020
+	AsymmetricSignPss       Capability = 0x0000000000000040
+	AsymmetricSignEcdsa     Capability = 0x0000000000000080
+	AsymmetricSignEddsa     Capability = 0x0000000000000100
+	AsymmetricDecryptPkcs   Capability = 0x0000000000000200
+	AsymmetricDecryptOaep   Capability = 0x0000000000000400
+	AsymmetricDecryptEcdh   Capability = 0x0000000000000800
+	ExportWrapped           Capability = 0x0000000000001000
+	ImportWrapped           Capability = 0x0000000000002000
+	PutWrapKey              Capability = 0x0000000000004000
+	GenerateWrapKey         Capability = 0x0000000000008000
+	ExportUnderWrap         Capability = 0x0000000000010000
+	PutOption               Capability = 0x0000000000020000
+	GetOption               Capability = 0x0000000000040000
+	GetRandomness           Capability = 0x0000000000080000
+	PutHmacKey              Capability = 0x0000000000100000
+	HmacKeyGenerate         Capability = 0x0000000000200000
+	HmacData                Capability = 0x0000000000400000
+	HmacVerify              Capability = 0x0000000000800000
+	Audit                   Capability = 0x0000000001000000
+	SshCertify              Capability = 0x0000000002000000
+	GetTemplate             Capability = 0x0000000004000000
+	PutTemplate             Capability = 0x0000000008000000
+	Reset                   Capability = 0x0000000010000000
+	OtpDecrypt              Capability = 0x0000000020000000
+	OtpAeadCreate           Capability = 0x0000000040000000
+	OtpAeadRandom           Capability = 0x0000000080000000
+	OtpAeadRewrapFrom       Capability = 0x0000000100000000
+	OtpAeadRewrapTo         Capability = 0x0000000200000000
+	Attest                  Capability = 0x0000000400000000
+	PutOtpAeadKey           Capability = 0x0000000800000000
+	GenerateOtpAeadKey      Capability = 0x0000001000000000
+	WrapData                Capability = 0x0000002000000000
+	UnwrapData              Capability = 0x0000004000000000
+	DeleteOpaque            Capability = 0x0000008000000000
+	DeleteAuthKey           Capability = 0x0000010000000000
+	DeleteAsymmetric        Capability = 0x0000020000000000
+	DeleteWrapKey           Capability = 0x0000040000000000
+	DeleteHmacKey           Capability = 0x0000080000000000
+	DeleteTemplate          Capability = 0x0000100000000000
+	DeleteOtpAeadKey        Capability = 0x0000200000000000
+	ChangeAuthenticationKey Capability = 0x0000400000000000
+)
 
+const (
 	// Domains
 	Domain1  Domain = 0x0001
-	Domain2         = 0x0002
-	Domain3         = 0x0004
-	Domain4         = 0x0008
-	Domain5         = 0x0010
-	Domain6         = 0x0020
-	Domain7         = 0x0040
-	Domain8         = 0x0080
-	Domain9         = 0x0100
-	Domain10        = 0x0200
-	Domain11        = 0x0400
-	Domain12        = 0x0800
-	Domain13        = 0x1000
-	Domain14        = 0x2000
-	Domain15        = 0x4000
-	Domain16        = 0x8000
+	Domain2  Domain = 0x0002
+	Domain3  Domain = 0x0004
+	Domain4  Domain = 0x0008
+	Domain5  Domain = 0x0010
+	Domain6  Domain = 0x0020
+	Domain7  Domain = 0x0040
+	Domain8  Domain = 0x0080
+	Domain9  Domain = 0x0100
+	Domain10 Domain = 0x0200
+	Domain11 Domain = 0x0400
+	Domain12 Domain = 0x0800
+	Domain13 Domain = 0x1000
+	Domain14 Domain = 0x2000
+	Domain15 Domain = 0x4000
+	Domain16 Domain = 0x8000
 
 	// object types
 	TypeOpaque        ObjectType = 0x01
-	AuthenticationKey            = 0x02
-	AsymmetricKey                = 0x03
-	WrapKey                      = 0x04
-	HmacKey                      = 0x05
-	Template                     = 0x06
-	OtpAeadKey                   = 0x07
+	AuthenticationKey ObjectType = 0x02
+	AsymmetricKey     ObjectType = 0x03
+	WrapKey           ObjectType = 0x04
+	HmacKey           ObjectType = 0x05
+	Template          ObjectType = 0x06
+	OtpAeadKey        ObjectType = 0x07
 )
